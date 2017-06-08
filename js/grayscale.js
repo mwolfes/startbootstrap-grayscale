73d38e5c76d3593a6308ea825af6afd4cafaa68e
Jennifer Wettig <wettig@uni-mainz.de>
2017-06-08 18:14:38 +0200

    it's the final countdown dödödödö

--- js/grayscale.js
+++ js/grayscale.js
@@ -78,3 +78,31 @@ function init() {
         icon: image
     });
 }
+
+//---------------------- fancy schmancy countdown timer ----------------------//
+
+// set the date we're counting down to
+var target_date = new Date("July 22, 2017 15:00:00").getTime();
+ 
+// variables for time units
+var days, hours;
+ 
+// get tag element
+var countingdown = document.getElementById('countingdown');
+ 
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+ 
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+ 
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+    
+    hours = parseInt(seconds_left / 3600);
+     
+    // format countdown string + set tag value
+    countingdown.innerHTML = '<span class="daysnumber">' + days +  ' </span><span class="days">Days</span> <span class="hoursnumber">' + hours + ' </span><span class="hours">Hours</span> '  ;  
+}, 1000);
